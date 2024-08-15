0e3fd0fdb877ad1d901951b3b0f60d814fab3411
ibrahimabdullahibare <ibrahimabdullahibare@gail.com>
2024-08-15 10:38:00 -0900

    react-hooks-props-basics-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* Conditionally render the <p> tag only if bio exists and is not an empty string */}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* Add the <Links /> component here */}
+      <Links github={ props.github} linkedin={props.linkedin} />
     </div>
   );
 }
 
 export default About;
+
